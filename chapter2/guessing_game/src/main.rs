903f27c48f9aa701a00f80459476f33426fe0b13
khalil hadji <h-adji_tech@proton.me>
2025-06-26 09:21:37 +0200

    guessinggame

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please guess a number!");
+    let secret = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please enter a valid number !");
+                continue;
+            }
+        };
+        println!("You guessed {guess}");
+        match guess.cmp(&secret) {
+            Ordering::Less => println!(" too small"),
+            Ordering::Equal => {
+                println!("You Guessed right !");
+                break;
+            }
+            Ordering::Greater => println!("too big"),
+        }
+    }
 }
